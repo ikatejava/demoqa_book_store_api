1af24601a6ea244eb6317e61c4a432506c347c49
Kate Belova <belovakate10@gmail.com>
2023-09-06 11:56:14 +0300

    some improvements

--- src/test/java/com/demoqa/tests/TestData.java
+++ src/test/java/com/demoqa/tests/TestData.java
@@ -7,27 +7,36 @@ public class TestData {
             accountInvalidPassword = "$B00k-1over$",
             validPasswordForRegistration = "j&PSJ-Yz6D",
             invalidPasswordForRegistration = "qwerty12",
+
             requestLoginURN = "Authorized",
             requestGenerateTokenURN = "GenerateToken",
             requestRegistrationURN = "User",
             requestCheckAllBooksURN = "Books",
             requestCheckBookInfoURN = "Book?ISBN=",
+
             responseGenerateTokenWithWrongPasswordStatus = "Failed",
             responseGenerateTokenWithWrongPasswordResult = "User authorization failed.",
+
             responseCode200Status = "Success",
             responseCode200Result = "User authorized successfully.",
+
             responseCode1200 = "1200",
             responseCode1200Message = "UserName and Password required.",
+
             responseCode1204 = "1204",
             responseCode1204Message = "User exists!",
+
             responseCode1205 = "1205",
             responseCode1205Message = "ISBN supplied is not available in Books Collection!",
+
             responseCode1207 = "1207",
             responseCode1207Message = "User not found!",
+
             responseCode1300 = "1300",
             responseCode1300Message = "Passwords must have at least one non alphanumeric character, " +
                     "one digit ('0'-'9'), one uppercase ('A'-'Z'), one lowercase ('a'-'z'), " +
                     "one special character and Password must be eight characters or longer.",
+
             gitPocketGuideISBN = "9781449325862",
             gitPocketGuideTitle = "Git Pocket Guide",
             gitPocketGuideSubtitle = "A Working Introduction",
@@ -39,6 +48,7 @@ public class TestData {
                     "for new users, as well as a reference to common commands and procedures for those of you " +
                     "with Git exp",
             gitPocketGuideWebsite = "http://chimera.labs.oreilly.com/books/1230000000561/index.html",
+
             nonExistentISBN = "123456789";
 
     public static int
